ae70ccc9639ea3b299614bfaf15b735efd674f95
Kai T. Ohlhus <k.ohlhus@gmail.com>
2017-01-11 13:11:44 +0100

    maint: Refactor `testVSDP.m` and add example 'arch4' from SDPLIB.

--- vsdpTest.m
+++ vsdpTest.m
@@ -3,8 +3,10 @@ function tests = vsdpTest
 %
 %   About the tests:
 %
-%      example1  contained in the original documentation of vsdplow and vsdpup
-%      example2  contained in demovsdp
+%    - example1      contained in the original documentation of vsdplow and
+%                    vsdpup
+%    - example2      contained in demovsdp
+%    - SDPLIB_ARCH4  contained in demovsdp
 %
 %   Example:
 %
@@ -17,22 +19,15 @@ function tests = vsdpTest
 tests = functiontests(localfunctions);
 end
 
-function [blk,A,C,b,X,y,Z] = testMYSDPS_example1(testCase)
+function [blk,A,C,b] = example1()
 blk(1,:) = {'s'; 2};
 C{1}   = [1 0; 0 1];
 A{1,1} = [0 1; 1 0];
 A{2,1} = [1 1; 1 1];
 b = [1; 2.0001];
-[objt,X,y,Z,info] = mysdps(blk,A,C,b);
-verifyEqual(testCase, objt, [1, 1], 'RelTol', 1e-4)
-verifyEqual(testCase, X{1}, ones(2)/2, 'RelTol', 1e-4)
-verifyEqual(testCase, objt(1), trace(C{1} * X{1}), 'RelTol', 1e-4)
-verifyEqual(testCase, objt(2), b'*y, 'RelTol', 1e-4)
-verifyEqual(testCase, Z{1}, C{1} - A{1,1}*y(1) - A{2,1}*y(2), 'RelTol', 1e-4)
-verifyEqual(testCase, info, 0)
 end
 
-function [blk,A,C,b,X,y,Z] = testMYSDPS_example2(testCase)
+function [blk,A,C,b,DELTA] = example2()
 DELTA = 1e-4;
 C{1} = [ 0   1/2    0;
   1/2 DELTA   0;
@@ -51,6 +46,55 @@ A{4,1} = [0 0 0;
   0 1 0];
 b = [1; 2*DELTA; 0; 0];
 blk(1,:) = {'s'; 3};
+end
+
+function testVSDPCHECK_example1(testCase)
+[blk,A,C,b] = example1();
+[m,n] = vsdpcheck(blk,A,C,b);
+verifyEqual(testCase, m, 2)
+verifyEqual(testCase, n, 1)
+end
+
+function testVSDPCHECK_example2(testCase)
+[blk,A,C,b,~] = example2();
+[m,n] = vsdpcheck(blk,A,C,b);
+verifyEqual(testCase, m, 4)
+verifyEqual(testCase, n, 1)
+end
+
+function testSDPT3_conversion_example1(testCase)
+[blk,A,C,b] = example1();
+[At,Ct] = vsdp_to_sdpt3(blk,A,C,b);
+[AA,CC] = sdpt3_to_vsdp(blk,At,Ct,b);
+for i = 1:length(b)
+  verifyEqual(testCase, full(AA{i,1}), A{i,1})
+end
+verifyEqual(testCase, CC, C)
+end
+
+function testSDPT3_conversion_example2(testCase)
+[blk,A,C,b,~] = example2();
+[At,Ct] = vsdp_to_sdpt3(blk,A,C,b);
+[AA,CC] = sdpt3_to_vsdp(blk,At,Ct,b);
+for i = 1:length(b)
+  verifyEqual(testCase, full(AA{i,1}), A{i,1})
+end
+verifyEqual(testCase, CC, C)
+end
+
+function [blk,A,C,b,X,y,Z] = testMYSDPS_example1(testCase)
+[blk,A,C,b] = example1();
+[objt,X,y,Z,info] = mysdps(blk,A,C,b);
+verifyEqual(testCase, objt, [1, 1], 'RelTol', 1e-4)
+verifyEqual(testCase, X{1}, ones(2)/2, 'RelTol', 1e-4)
+verifyEqual(testCase, objt(1), trace(C{1} * X{1}), 'RelTol', 1e-4)
+verifyEqual(testCase, objt(2), b'*y, 'RelTol', 1e-4)
+verifyEqual(testCase, Z{1}, C{1} - A{1,1}*y(1) - A{2,1}*y(2), 'RelTol', 1e-4)
+verifyEqual(testCase, info, 0)
+end
+
+function [blk,A,C,b,X,y,Z,DELTA] = testMYSDPS_example2(testCase)
+[blk,A,C,b,DELTA] = example2();
 [objt,X,y,Z,info] = mysdps(blk,A,C,b);
 verifyEqual(testCase, objt, [-0.5 -0.5], 'RelTol', DELTA)
 testX = X{1};
@@ -78,8 +122,7 @@ verifyEqual(testCase, lb, 5e-5, 'RelTol', 1e-4)
 end
 
 function testVSDPUP_example2(testCase)
-DELTA = 1e-4;
-[blk,A,C,b,Xt,yt,Zt] = testMYSDPS_example2(testCase);
+[blk,A,C,b,Xt,yt,Zt,DELTA] = testMYSDPS_example2(testCase);
 [fU,X,lb] = vsdpup(blk,A,C,b,Xt,yt,Zt);
 verifyEqual(testCase, fU, -0.5, 'RelTol', DELTA)
 midX = [2*DELTA, -1, 0; -1, 1/(2*DELTA), 0; 0, 0, DELTA];
@@ -88,8 +131,7 @@ verifyEqual(testCase, lb, 1.289076539560735e-8, 'AbsTol', DELTA)
 end
 
 function testVSDPUP_example2_finite_bnd(testCase)
-DELTA = 1e-4;
-[blk,A,C,b,Xt,yt,Zt] = testMYSDPS_example2(testCase);
+[blk,A,C,b,Xt,yt,Zt,DELTA] = testMYSDPS_example2(testCase);
 yu = 1e5 * [1 1 1 1]';
 [fU,X,lb] = vsdpup(blk,A,C,b,Xt,yt,Zt,yu);
 % In this case: VSDPUP computes rigorously the finite upper bound without
@@ -108,8 +150,7 @@ verifyEqual(testCase, dl, 9.3912e-11, 'AbsTol', 1e-9)
 end
 
 function testVSDPLOW_example2(testCase)
-DELTA = 1e-4;
-[blk,A,C,b,Xt,yt,Zt] = testMYSDPS_example2(testCase);
+[blk,A,C,b,Xt,yt,Zt,DELTA] = testMYSDPS_example2(testCase);
 [fL,Y,dl] = vsdplow(blk,A,C,b,Xt,yt,Zt);
 verifyEqual(testCase, fL, -0.5, 'RelTol', DELTA)
 verifyEqual(testCase, Y(2), -1/(4*DELTA), 'RelTol', DELTA)
@@ -118,8 +159,7 @@ verifyEqual(testCase, dl, 4.319659051028185e-13, 'AbsTol', 1e-9)
 end
 
 function testVSDPLOW_example2_finite_bnd(testCase)
-DELTA = 1e-4;
-[blk,A,C,b,Xt,yt,Zt] = testMYSDPS_example2(testCase);
+[blk,A,C,b,Xt,yt,Zt,DELTA] = testMYSDPS_example2(testCase);
 xu = 1e5;
 % FIXME: tiny pertubation neccessary to work with approximation from SDPT3-4.0
 yt(2) = yt(2) - DELTA/4;
@@ -128,4 +168,48 @@ verifyEqual(testCase, fL, -0.5, 'RelTol', DELTA)
 verifyEqual(testCase, Y(2), -1/(4*DELTA), 'RelTol', DELTA)
 verifyEqual(testCase, Y([1,3,4]), [0;0;0], 'AbsTol', DELTA)
 verifyEqual(testCase, dl, 4.319659051028185e-13, 'AbsTol', 1e-9)
+end
+
+function testSDPLIB_ARCH4(testCase)
+n = 2;
+m = 174;
+s = [161; 174];
+% test sdpa_to_vsdp
+[blk,A,C,b] = sdpa_to_vsdp(fullfile('sdplib_vsdp', 'arch4.dat-s'));
+verifyEqual(testCase, blk, {'s', s(1); 's', s(2)})
+verifyEqual(testCase, length(b), m)
+for j = 1:n
+  verifyEqual(testCase, size(C{j}), [s(j), s(j)])
+  for i = 1:m
+    verifyEqual(testCase, size(A{i,j}), [s(j), s(j)])
+  end
+end
+% test vsdpcheck
+[m,n] = vsdpcheck(blk,A,C,b);
+verifyEqual(testCase, m, 174)
+verifyEqual(testCase, n, 2)
+% test SDPT3 conversion
+[At,Ct] = vsdp_to_sdpt3(blk,A,C,b);
+[AA,CC] = sdpt3_to_vsdp(blk,At,Ct,b);
+verifyEqual(testCase, CC, C)
+verifyEqual(testCase, AA, A, 'RelTol', 1e-8)
+% test SDPT3 mysdps
+[objt,Xt,yt,Zt,info] = mysdps(blk,A,C,b);
+verifyEqual(testCase, size(objt), [1, 2])
+verifyEqual(testCase, size(Xt), [2, 1])
+verifyEqual(testCase, size(yt), size(b))
+verifyEqual(testCase, size(Zt), [2, 1])
+verifyEqual(testCase, objt(1), trace(C{1} * Xt{1}), 'RelTol', 1e-4)
+verifyEqual(testCase, objt(2), b'*yt, 'RelTol', 1e-4)
+verifyEqual(testCase, info, 0)
+% test SDPT3 vsdpup
+[fU,X,lb] = vsdpup(blk,A,C,b,Xt,yt,Zt);
+verifyEqual(testCase, objt(2) <= fU, true)
+verifyEqual(testCase, isscalar(X) && isnan(X), false);
+verifyEqual(testCase, any(isnan(lb)), false);
+% test SDPT3 vsdplow
+[fL,Y,dl] = vsdplow(blk,A,C,b,Xt,yt,Zt);
+verifyEqual(testCase, fL <= objt(1), true)
+verifyEqual(testCase, isscalar(Y) && isnan(Y), false);
+verifyEqual(testCase, any(isnan(dl)), false);
 end
