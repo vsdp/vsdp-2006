f34397326262274f05a9060d3b1a4828a8d2933b
Kai T. Ohlhus <k.ohlhus@gmail.com>
2017-01-13 15:05:43 +0100

    doc: add files for Matlab toolbox distribution.

---
+++ Contents.m
@@ -0,0 +1,19 @@
+% VSDP-2006-NG
+%
+% Files
+%   demovsdp            - Demonstration of VSDP: Verified SemiDefinite Programming
+%   mysdps              - My Semidefinite Programming solver for the block-diagonal problem:
+%   SDP_GLOBALPARAMETER - Copyright 2004-2006 Christian Jansson (jansson@tuhh.de)
+%   sdpa_to_vsdp        - Import problem in SDPA sparse format to VSDP's format.
+%   sdpt3_to_vsdp       - Convert SDPT3 to VSDP format.
+%   veigsym             - Verified enclosure for all eigenvalues of a symmetric matrix.
+%   vsdp_to_sdpt3       - Convert VSDP to SDPT3 format.
+%   vsdpcheck           - Routine for checking the VSDP format.
+%   vsdpinfeas          - Infeasibility-check for the block-diagonal problem:
+%   vsdplow             - Rigorous upper bound for the min. value of the block-diagonal problem:
+%   vsdpTest            - Runs a Matlab testsuite for VSDP (version 2006).
+%   vsdpup              - Rigorous upper bound for the min. value of the block-diagonal problem:
+%   vsmat               - Inverse operation of 'vsvec'.
+%   vsvec               - Vectorize a symmetric block diagonal matrix.
+%   vuls                - Verification for underdetermined linear systems.
+%
